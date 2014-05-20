59cb99247d2e9917c84683c73928fb9bbe8c7c1e
Vinci <vincicat@gmail.com>
2014-05-21 01:11:06 +0800

    Experimental Router

--- client/views/location/location.js
+++ client/views/location/location.js
@@ -3,6 +3,11 @@ Template.location.helpers({
     return Companies.findOne({_id: this.company_id}); 
   },
   products: function(){
-    return Products.find({_id: { $in: this.product_ids }});
+    if (this.product_ids && this.product_ids.length > 0) {
+      return Products.find({_id: { $in: this.product_ids }});
+    } else {
+      console.log(this.product_ids)
+      return [];
+    }
   }
 });
