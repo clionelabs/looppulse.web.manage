5eed93cfd819b45c70284b8f0aaab28c20733bbd
Thomas Pun <thomas@pun.io>
2014-05-22 15:04:58 +0800

    remove Session for now

--- server/main.js
+++ server/main.js
@@ -41,8 +41,7 @@ var buildDemoData = function() {
     var companyName = 'Marathon Sports';
     company = new Company(companyName, 'http://www.ilovelkf.hk/sites/www.ilovelkf.hk/files/business/image_promo/marathon-sports-logo-promo.png');
     company.save();
-    Session.set('company_id', company._id);
-    
+
     var location = new Location('Causeway Bay Store', 'Shop 616, L6, Times Squaocaocnre, Causeway Bay', company._id);
     location.save();
 
@@ -51,11 +50,14 @@ var buildDemoData = function() {
                    {uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', major: 54330, minor: 38700},
                    {uuid: 'E2C56DB5-DFFB-48D2-B060-D0F5A71096E0', major: 10,    minor: 47}];
     demoProducts.forEach(function(element, index, array) {
-      console.log("creating product");
-      var product = new Product(element, location._id);
+      var product = new Product(element, company._id);
       product.save();
 
-      var beacon = new Beacon(demoBeacons[index].uuid, demoBeacons[index].major, demoBeacons[index].minor, product._id);
+      var beacon = new Beacon(demoBeacons[index].uuid,
+                              demoBeacons[index].major,
+                              demoBeacons[index].minor,
+                              product._id,
+                              location._id);
       beacon.save();
     });
   }
