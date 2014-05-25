afa0de4afe5087e0854388834cd833eb1507b9cc
Thomas Pun <thomas@pun.io>
2014-05-25 12:20:27 +0800

    first working version of Location#funnel #7

--- collections/locations.js
+++ collections/locations.js
@@ -12,64 +12,65 @@ Location.prototype.save = function() {
   return this._id;
 }
 
-Location.prototype.entranceInstallationIds = function() {
-  Installations.find({locationId: _id, type: 'entrance'}).map(
-    function(installation, index, cursor) {
-      return installation._id;
-    }
-  );
+Location.load = function(id) {
+  var json = Locations.findOne({_id: id});
+  var loaded = new Location(json.name, json.address, json.companyId);
+  loaded._id = json._id;
+  return loaded;
 }
 
-Location.prototype.productInstallationId= function(productId) {
-  Installations.find({locationId: _id, type: 'product', physicalId: productId}).map(
-    function(installation, index, cursor) {
-      return installation._id;
-    }
-  );
+var mapId = function(object, index, cursor) {
+  return object._id;
+}
+
+Location.prototype.entranceInstallationIds = function() {
+  var installs = Installations.find({locationId: this._id, type: 'entrance'});
+  return Installations.find({locationId: this._id, type: 'entrance'}).map(mapId);
+}
+
+Location.prototype.productInstallationId = function(productId) {
+  return Installations.find({locationId: this._id,
+                             type: 'product',
+                             physicalId: productId}).map(mapId);
 }
 
 Location.prototype.cashierInstallationIds = function() {
-  Installations.find({locationId: _id, type: 'cashier'}).map(
-    function(installation, index, cursor) {
-      return installation._id;
-    }
-  );
+  return Installations.find({locationId: this._id, type: 'cashier'}).map(mapId);
 }
-
-
 
 Location.prototype.funnel = function(productId, timeRange) {
   var funnel = {entrances: 0, product: 0, cashiers: 0};
-  var timeStart = timeRange.start;
-  var timeEnd = timeRange.end;
 
   // Entrance
-  entranceEncounters = Encounters.find({installationId: {$in: this.entranceInstallationIds},
-                                        enteredAt: {$gte: timeStart, $lt: timeEnd},
-                                        exitedAt:  {$gte: timeStart, $lt: timeEnd}});
+  var entranceEncounters = Encounters.find({installationId: {$in: this.entranceInstallationIds()},
+                                            enteredAt: timeRange,
+                                            exitedAt:  timeRange });
   funnel.entrances = entranceEncounters.count();
 
-  // Return if we couldnt' find given product
-  if (!Installations.findOne({locationId:this._id, physicalId: productId, type: 'product'})) {
+  // Return if we couldn't find given product
+  var productInstallation = Installations.findOne({locationId:this._id,
+                                                   type: 'product',
+                                                   physicalId: productId});
+  if (!productInstallation) {
     console.log('Cannot find product with ID: ' + productId);
     return funnel;
   }
 
   // Product
-  productEncounters = Encounters.find({installationId: {$in: this.productInstallationId(productId)},
-                                       enteredAt: {$gte: timeStart, $lt: timeEnd},
-                                       exitedAt:  {$gte: timeStart, $lt: timeEnd}});
+  var productEncounters = Encounters.find({installationId: productInstallation._id,
+                                           enteredAt: timeRange,
+                                           exitedAt:  timeRange});
   funnel.product = productEncounters.count();
 
   // Cashier after visiting Product
-  visitorIds = productEncounters.map(
-    function(encounter, index, cursor) {
-      return encounter.visitorId;
-    }
-  );
-  funnel.cashiers = Encounters.find({installationId: {$in: this.cashierInstallationIds},
-                                     visitorId: {$in: visitorIds},
-                                     enteredAt: {$gte: timeStart, $lt: timeEnd},
-                                     exitedAt:  {$gte: timeStart, $lt: timeEnd}});
+  var visitorIds = productEncounters.map(function(encounter, index, cursor) {
+                                           return encounter.visitorId;
+                                         });
+  var cashierEncounters = Encounters.find({installationId: {$in: this.cashierInstallationIds()},
+                                           visitorId: {$in: visitorIds},
+                                           enteredAt: timeRange,
+                                           exitedAt:  timeRange});
+  funnel.cashiers = cashierEncounters.count();
+
   return funnel;
 }
