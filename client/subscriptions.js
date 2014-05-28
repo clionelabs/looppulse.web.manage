e9f2a5dc81399303052b40d03146f1625af6a4a1
Vinci <vincicat@gmail.com>
2014-05-28 23:51:25 +0800

    WIP #21: fix the publication & subscription

--- client/subscriptions.js
+++
@@ -1,3 +0,0 @@
-// Meteor.subscribe('companies');
-// Meteor.subscribe('locations');
-// Meteor.subscribe('products');
--- server/publications.js
+++ server/publications.js
@@ -1,23 +1,43 @@
-Meteor.publish('company', function(_id) {
-  return Companies.findOne(_id);
+DEBUG = true
+console.log("publishers")
+
+Meteor.publish('owned-company', function(_id) {
+  q = {}
+  if (_id) {
+    q = {_id:_id}
+  } else if (DEBUG) {
+    q = {}
+  } else {
+    return null;
+  }
+  return Companies.find(q); //Note: Return MongoDB Cursor
+
 });
 
-Meteor.publish('locations', function(company_id) {
-  return Locaitons.find({company_id: company_id});
+Meteor.publish('owned-locations', function(companyId) {
+  q = {}
+  if (companyId) {
+    q = {companyId:companyId}
+  } else if (DEBUG) {
+    q = {}
+  } else {
+    return null;
+  }
+  return Locations.find(q);
 });
 
-Meteor.publish('location', function(_id) {
-  return Locations.findOne(_id);
+// Meteor.publish('location', function(_id) {
+//   return Locations.findOne(_id);
+// });
+
+Meteor.publish('beacons', function(_id) {
+  // var products = Products.find({location_id: location_id});
+  // return products.map(function(product, index, ref) {
+  //   Beacons.findOne({product_id: product._id},
+  //                   {fields: {uuid: 0, major: 0, minor: 0}});
+  // });
 });
 
-Meteor.publish('beacons', function(location_id) {
-  var products = Products.find({location_id: location_id});
-  return products.map(function(product, index, ref) {
-    Beacons.findOne({product_id: product._id},
-                    {fields: {uuid: 0, major: 0, minor: 0}});
-  });
-});
-
-Meteor.publish('encounters', function(beacon_id) {
-  return Encounters.find({beacon_id: beacon_id});
-});
+// Meteor.publish('encounters', function(beacon_id) {
+//   return Encounters.find({beacon_id: beacon_id});
+// });
