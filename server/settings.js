0a8e0b7befcdad0ca0c8f45c51feb2f350672c68
Thomas Pun <thomas@pun.io>
2014-06-01 12:11:11 +0800

    correct path for settings.json in help text

--- server/settings.js
+++ server/settings.js
@@ -1,6 +1,6 @@
 setDefaultSettings = function() {
   if (JSON.stringify(Meteor.settings)=='{}') {
-    console.log("Meteor.settings expected. Rerun: meteor --settings ../server/settings.json");
+    console.log("Meteor.settings expected. Rerun: meteor --settings server/settings.json");
 
     // We can try to read the file using
     // https://gist.github.com/awatson1978/4625493
