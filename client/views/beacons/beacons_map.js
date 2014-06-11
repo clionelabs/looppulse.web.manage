e267ae31b0dc89ad4edcd139f122620ec7c5a0a1
Vinci <vincicat@gmail.com>
2014-06-11 19:14:13 +0800

    updated map with some limiter to avoid overload the client side (WIP#33)

--- client/views/beacons/beacons_map.js
+++ client/views/beacons/beacons_map.js
@@ -9,7 +9,6 @@ Template.beacons_map.helpers({
     var padding = 45;
     var w = 583 - padding;
     var h = 425 - padding;
-    console.log("Beacon", this)
     return { x: Template.beacons_map.randInt(padding, w), y: Template.beacons_map.randInt(padding, h) };
   },
   time: function(ts, tz){
@@ -22,9 +21,13 @@ Template.beacons_map.helpers({
 });
 Template.beacons_map_marker.helpers({
   events: function(){
-    var events = BeaconEvents.find({ beaconId: this.beacon.beaconId,  type: "didRangeBeacons" }).map(function(o, idx) {
-      //console.log(o, idx)
+    var lim = 300;
+    var t = (new Date()).getTime();
+    var qt = t - (10 * 60 * 1000); // js timestamp is in ms
+    var events = BeaconEvents.find({ beaconId: this.beacon.beaconId,  type: "didRangeBeacons", createdAt: { $gt: qt } }, {limit: lim }).map(function(o, idx) {
+      console.log(o._id, new Date(o.createdAt), idx)
       o.pos = idx +1
+      o.delay = idx *1000 + 200
       return o;
     });
     return events;
