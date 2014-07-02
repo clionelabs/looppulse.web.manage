9365d7ec35146dbaf8585bbdf4df734f89d2c86e
Vinci <vincicat@gmail.com>
2014-07-02 23:09:33 +0800

    Improved pulsing #46
    Note: currently removed the query limit as observe change doesn't allowed `limit` modifier

--- client/views/beacons/beacons_map.js
+++ client/views/beacons/beacons_map.js
@@ -17,58 +17,48 @@ Template.beacons_map.helpers({
   }
 });
 Template.beacons_map.rendered = function(){
-  // console.log(this)
-  var self = this;
-  self.marker = 0;
 
-  var pulsing = function($elem) {
-    $elem.find(".pulse").first().addClass("on")
-  }
-
-  $('.beacon-map').on('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', '.beacon-marker', function(e){
-    //target: ring
-    //current target: marker
-    //delegate: map
-    var marker = e.currentTarget;
-    $(".on", marker).removeClass("on");
-    pulsing($(marker))
-  }).on("beacon-set beacon-rendered", function(e, beaconId){
-    pulsing($("[data-beacon-id='"+beaconId+"']", e.target))
-  });
 }
 Template.beacons_map_marker.prefix = "beacon-";
+Template.beacons_map_marker.owlMode = true;
 Template.beacons_map_marker.created = function(){
+
+  //jQuery('.beacon-map').trigger("beacon-set", beaconId)
+};
+Template.beacons_map_marker.rendered = function(){
+  this.domReady = true;
+  this.processing = false;
+  //console.log("marker rendered", this.data.beacon.beaconId)
+  //jQuery('.beacon-map').trigger("beacon-rendered", beaconId)
   var beaconId = this.data.beacon.beaconId
   //console.log("Setting Up Query Listener", this, beaconId)
   var config = Meteor.settings.public.view.beaconsMap || {};
   var lim = config.limit || 10;
   var now = (new Date()).getTime();
   var t = now - (config.forwardBufferInSec * 60 * 1000); // js timestamp is in ms
-  var _watch = BeaconEvents.find({ beaconId: beaconId,  type: "didRangeBeacons", createdAt: { $gt: t } })；
+  var _watch = BeaconEvents.find({ beaconId: beaconId,  type: "didRangeBeacons", createdAt: { $gt: t } });
   var tmpl = this;
   var handle = null;
-  console.log(t, this);
-  this.processing = false;
   handle = _watch.observeChanges({
       added: function(id, fields){
         // We do something when new event get in
         var $container = jQuery(".beacon-map");
         var qn = Template.beacons_map_marker.prefix + beaconId;
         var qs = $container.queue(qn).length;
-        console.log(id, "➣", qn, qs)
+        if(!Template.beacons_map_marker.owlMode){ console.log(id, "➣", qn, qs) }
         $container.queue(qn, function() {
-          var self = this; //container dom node
-          var _qn = qn;
-          var _beaconId = beaconId;
-          //console.log(next);
+          if(!Template.beacons_map_marker.owlMode){ console.log("Running", qn) }
           tmpl.processing = true;
-          setTimeout(function() {
-            tmpl.processing = false;
-            console.log("End", _beaconId);
-            $(self).dequeue(_qn);
-          }, 1200)
+          var self = this; //container dom node
+          var _beaconId = beaconId;
+          var marker = $(".beacon-marker[data-beacon-id='"+_beaconId+"']");
+          // avoid blocking.
+          setTimeout(function(){
+            $(marker).find(".pulse").addClass("on").append('<div class="ring"></div>'); //avoid caching
+          }, 100)
         });
-        if(!tmpl.processing & tmpl.domReady){
+        if(!tmpl.processing && qs === 0){
+          console.log("interrupt")
           tmpl.processing = true;
           $container.dequeue(qn);
 
@@ -76,14 +66,17 @@ Template.beacons_map_marker.created = function(){
       }
   });
   this.handle = handle;
+  var $marker = $(".beacon-marker[data-beacon-id='"+beaconId+"']");
+  var qn = Template.beacons_map_marker.prefix + beaconId;
 
-  //jQuery('.beacon-map').trigger("beacon-set", beaconId)
-};
-Template.beacons_map_marker.rendered = function(){
-  //console.log("marker rendered", this.data.beacon.beaconId)
-  var beaconId = this.data.beacon.beaconId;
-  this.domReady = true;
-  //jQuery('.beacon-map').trigger("beacon-rendered", beaconId)
+  $marker.on(animationEnd, ".pulse", function(e){
+    if(!Template.beacons_map_marker.owlMode){ console.log("Next:", qn) }
+    $(".on .ring", e.target).remove()
+    setTimeout(function(){
+      tmpl.processing = false;
+      $(".beacon-map").dequeue(qn)
+    }, 100)
+  })
 };
 
 Template.beacons_map_marker.destroyed = function(){
