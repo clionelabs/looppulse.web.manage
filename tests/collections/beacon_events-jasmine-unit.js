fab2671cc8e6dc5202d50ad5d8b7578326d679bd
Ratsolean <rat.so.nlp+github@gmail.com>
2014-07-23 23:00:37 +0800

    Add a simple test to demo jasmine unit test

---
+++ tests/collections/beacon_events-jasmine-unit.js
@@ -0,0 +1,18 @@
+(function() {
+
+    "use strict";
+
+    describe("BeaconEvents", function () {
+        beforeEach(function () {
+        });
+
+        it("should return false when missing proximity", function () {
+            var beacon_event = new BeaconEvent('aVisitorId', 'aBeaconId', {
+                type: 't1'
+            });
+            expect(beacon_event.warnAboutUnknownProximity()).toBe(false);
+        });
+
+    });
+
+}());
