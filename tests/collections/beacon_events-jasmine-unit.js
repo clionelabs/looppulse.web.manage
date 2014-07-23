05eda955326b1d2938dd21cc0a959bbf80299788
Ratsolean <rat.so.nlp+github@gmail.com>
2014-07-23 22:45:46 +0800

    Use 2 white-space indentation

--- tests/collections/beacon_events-jasmine-unit.js
+++ tests/collections/beacon_events-jasmine-unit.js
@@ -1,18 +1,18 @@
-(function() {
+(function () {
 
-    "use strict";
+  "use strict";
 
-    describe("BeaconEvents", function () {
-        beforeEach(function () {
-        });
-
-        it("should return false when missing proximity", function () {
-            var beacon_event = new BeaconEvent('aVisitorId', 'aBeaconId', {
-                type: 't1'
-            });
-            expect(beacon_event.warnAboutUnknownProximity()).toBe(false);
-        });
-
+  describe("BeaconEvents", function () {
+    beforeEach(function () {
     });
+
+    it("should return false when missing proximity", function () {
+      var beacon_event = new BeaconEvent('aVisitorId', 'aBeaconId', {
+        type: 't1'
+      });
+      expect(beacon_event.warnAboutUnknownProximity()).toBe(false);
+    });
+
+  });
 
 }());
