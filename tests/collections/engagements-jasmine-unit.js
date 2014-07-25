f18be80b07af8e4c715e024baeeae4b321b48cb7
Ratsolean <rat.so.nlp+github@gmail.com>
2014-07-25 15:31:07 +0800

    Test Engagement.dispatch

--- tests/collections/engagements-jasmine-unit.js
+++ tests/collections/engagements-jasmine-unit.js
@@ -16,6 +16,33 @@
       });
     });
 
+    describe("dispatch()", function () {
+      it("should not trigger encounter when not ready", function () {
+        spyOn(Installations, 'findOne').andReturn({ localtionId: 'aLocationId' });
+        var fakeEngagement = jasmine.createSpyObj('engagement', ['readyToTrigger', 'trigger']);
+        spyOn(Engagement, 'availableEngagements').andReturn([fakeEngagement]);
+        var expectedEncounter = {};
+
+        Engagement.dispatch(expectedEncounter);
+
+        expect(fakeEngagement.readyToTrigger).toHaveBeenCalledWith(expectedEncounter);
+        expect(fakeEngagement.trigger).not.toHaveBeenCalled();
+      });
+
+      it("should trigger encounter", function () {
+        spyOn(Installations, 'findOne').andReturn({ localtionId: 'aLocationId' });
+        var fakeEngagement = jasmine.createSpyObj('engagement', ['readyToTrigger', 'trigger']);
+        fakeEngagement.readyToTrigger.andReturn(true);
+        spyOn(Engagement, 'availableEngagements').andReturn([fakeEngagement]);
+        var expectedEncounter = {};
+
+        Engagement.dispatch(expectedEncounter);
+
+        expect(fakeEngagement.readyToTrigger).toHaveBeenCalledWith(expectedEncounter);
+        expect(fakeEngagement.trigger).toHaveBeenCalledWith(expectedEncounter);
+      });
+    });
+
   });
 
 }());
