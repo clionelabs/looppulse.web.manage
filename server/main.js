816e3086a15fbc5ee682f9949117ce73e37af09a
Ratsolean <rat.so.nlp+github@gmail.com>
2014-08-01 11:42:43 +0800

    Time startup tasks

--- server/main.js
+++ server/main.js
@@ -1,12 +1,24 @@
 Meteor.startup(
-  Meteor.bindEnvironment(function(){
+  Meteor.bindEnvironment(function () {
+    console.time("[startup] configure");
     configure();
+    console.timeEnd("[startup] configure");
 
+    console.time("[startup] ensureIndexes");
     ensureIndexes();
+    console.timeEnd("[startup] ensureIndexes");
+    console.time("[startup] observeCompaniesFromFirebase");
     observeCompaniesFromFirebase();
+    console.timeEnd("[startup] observeCompaniesFromFirebase");
+    console.time("[startup] observeBeaconEventsFromFirebase");
     observeBeaconEventsFromFirebase();
+    console.timeEnd("[startup] observeBeaconEventsFromFirebase");
 
+    console.time("[startup] Encounter");
     Encounter.startup();
+    console.timeEnd("[startup] Encounter");
+    console.time("[startup] Engagement");
     Engagement.startup();
+    console.timeEnd("[startup] Engagement");
   })
 );
