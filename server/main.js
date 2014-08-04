75f596fd84e9abdcadf96c3827fd604310dd0861
Ratsolean <rat.so.nlp+github@gmail.com>
2014-08-04 16:08:52 +0800

    Defer startup tasks. Close #65

--- server/main.js
+++ server/main.js
@@ -1,24 +1,26 @@
 Meteor.startup(
   Meteor.bindEnvironment(function () {
-    console.time("[startup] configure");
-    configure();
-    console.timeEnd("[startup] configure");
+    Meteor.defer(function () {
+      console.time("[startup] configure");
+      configure();
+      console.timeEnd("[startup] configure");
 
-    console.time("[startup] ensureIndexes");
-    ensureIndexes();
-    console.timeEnd("[startup] ensureIndexes");
-    console.time("[startup] observeCompaniesFromFirebase");
-    observeCompaniesFromFirebase();
-    console.timeEnd("[startup] observeCompaniesFromFirebase");
-    console.time("[startup] observeBeaconEventsFromFirebase");
-    observeBeaconEventsFromFirebase();
-    console.timeEnd("[startup] observeBeaconEventsFromFirebase");
+      console.time("[startup] ensureIndexes");
+      ensureIndexes();
+      console.timeEnd("[startup] ensureIndexes");
+      console.time("[startup] observeCompaniesFromFirebase");
+      observeCompaniesFromFirebase();
+      console.timeEnd("[startup] observeCompaniesFromFirebase");
+      console.time("[startup] observeBeaconEventsFromFirebase");
+      observeBeaconEventsFromFirebase();
+      console.timeEnd("[startup] observeBeaconEventsFromFirebase");
 
-    console.time("[startup] Encounter");
-    Encounter.startup();
-    console.timeEnd("[startup] Encounter");
-    console.time("[startup] Engagement");
-    Engagement.startup();
-    console.timeEnd("[startup] Engagement");
+      console.time("[startup] Encounter");
+      Encounter.startup();
+      console.timeEnd("[startup] Encounter");
+      console.time("[startup] Engagement");
+      Engagement.startup();
+      console.timeEnd("[startup] Engagement");
+    });
   })
 );
