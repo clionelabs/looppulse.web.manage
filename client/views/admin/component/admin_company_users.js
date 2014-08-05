7861a63c14e800ca7322ff33e7e16e98585d9735
Vinci <vincicat@gmail.com>
2014-08-05 19:03:41 +0800

    Various fix
    - fix the subscription
    - update the meteor for the reative table.

--- client/views/admin/component/admin_company_users.js
+++ client/views/admin/component/admin_company_users.js
@@ -2,15 +2,6 @@ Template.admin_company_users.helpers({
   currentCompanyUser: function(){
     if (!this || !this.data) { return; }
     var data = this.data;
-
-    console.log(this, data)
     return Meteor.users.find({"profile.Companies": data._id}, {fields:{"emails.address":1}})
-  },
-  settings: function () {
-         return {
-             rowsPerPage: 10,
-             showFilter: true,
-             fields: ['_id', "emails.0.address"]
-         };
   }
 })
