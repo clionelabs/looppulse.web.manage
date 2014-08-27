7b116193f9e6b0565d9fdcd8122cae4081d452c7
Vinci <vincicat@gmail.com>
2014-08-28 01:25:18 +0800

    Merge fix #87: provide dummy data for demo purpose.

--- client/views/dashboard/home.js
+++ client/views/dashboard/home.js
@@ -22,30 +22,8 @@ Template.dashboard_home.helpers({
     var campaigns = [];
     var collection;
 
-    /* TODO: extract the campaign data from engagement
-    collection = {}.find({})
-    collection.map(function(){
-      return {
-      _id: _id,
-      name: "",
-      sent: 0,
-      visited: 0,
-      conversion: 0
-      }
-    })
-    campaigns = collection
-    return campaigns;
-    */
-
-    //return dummy
+    //@@DEMO: return real stuff or dummy
     //@@WARNING: ORDER IMPORTANTED
-    /*
-    return [
-      { _id:"demo0001", name:"Elephant Parade", startTime: "", endTime: "", traceOffset: 100000, desc: "Visit the elephant on UG and a chance to win 2 movie tickets Engage customers when it’s Monday, Wednesday between 7pm to 9pm When they’re entering zone 107", sent:1000, viewed: 900, visited: 600, conversion: 0.6,viewConversion: 0.68  },
-      { _id:"demo0002", name:"Super Weekend", startTime: "", endTime: "", traceOffset: 100000, desc: "Lucky Draw on purchase > $500", sent:1200, viewed: 800,  visited: 600, conversion: 0.5, viewConversion: 0.75 },
-      { _id:"demo0003", name:"Summer Festival", startTime: "", endTime: "", traceOffset: 100000, desc: "10% off for everything", sent:2000, viewed: 1200, visited: 400, conversion: 0.2,  viewConversion: 0.33 }
-    ]
-    */
     Engagements.find().forEach(function(engagement) {
       var metric = EngagementMetrics.findOne({ engagementId: engagement._id });
       campaigns.push({
@@ -55,9 +33,18 @@ Template.dashboard_home.helpers({
         sent: metric.sentMessageCount,
         viewed: metric.viewedMessageCount,
         visited: metric.visitedCount,
-        conversion: metric.conversionRates.sentMessageToVisited
+        conversion: metric.conversionRates.sentMessageToVisited,
+        startTime: "", endTime: "", traceOffset: 100000
       });
     });
+
+    if (campaigns.length === 0){
+      return [
+        { _id:"demo0001", name:"Elephant Parade", startTime: "", endTime: "", traceOffset: 100000, desc: "Visit the elephant on UG and a chance to win 2 movie tickets Engage customers when it’s Monday, Wednesday between 7pm to 9pm When they’re entering zone 107", sent:1000, viewed: 900, visited: 600, conversion: 0.6,viewConversion: 0.68  },
+        { _id:"demo0002", name:"Super Weekend", startTime: "", endTime: "", traceOffset: 100000, desc: "Lucky Draw on purchase > $500", sent:1200, viewed: 800,  visited: 600, conversion: 0.5, viewConversion: 0.75 },
+        { _id:"demo0003", name:"Summer Festival", startTime: "", endTime: "", traceOffset: 100000, desc: "10% off for everything", sent:2000, viewed: 1200, visited: 400, conversion: 0.2,  viewConversion: 0.33 }
+      ]
+    }
     return campaigns;
   },
   totalVisits: function(period){
@@ -322,6 +309,7 @@ Template.dashboard_performance_chart.created = function(){
   Session.set("view-chart", true)
 }
 Template.dashboard_performance_chart.rendered = function(){
+
   var self = this;
   var chart = d4.charts.line()
     .outerWidth($('main .charting').width())
@@ -347,7 +335,7 @@ Template.dashboard_performance_chart.rendered = function(){
       })
 
   self.handle =  Deps.autorun(function () {
-    var data = Template.dashboard_home.performances();
+    var data = self.data;
     var parsedData = parser(data);
 
     d3.select('main .charting')
