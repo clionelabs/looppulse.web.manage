b3536dc01808a915fae4ede3d8c1c258121cec39
Ratsolean <rat.so.nlp+github@gmail.com>
2014-09-02 07:59:56 +0800

    Pass encounter in segment test

--- tests/collections/segment-jasmine-unit.js
+++ tests/collections/segment-jasmine-unit.js
@@ -5,25 +5,28 @@
   describe("new Segment()", function() {
     describe("match()", function() {
       it("should return false if rules is undefined", function() {
+        var encounter =jasmine.createSpy('encounter');
         var segment = new Segment({});
 
-        var result = segment.match();
+        var result = segment.match(encounter);
 
         expect(result).toBe(false);
       });
 
       it("should return false if rules is empty array", function() {
+        var encounter =jasmine.createSpy('encounter');
         var segment = new Segment({ rules: [] });
 
-        var result = segment.match();
+        var result = segment.match(encounter);
 
         expect(result).toBe(false);
       });
 
       it("should return true if \"visitors\" in rules", function() {
+        var encounter =jasmine.createSpy('encounter');
         var segment = new Segment({ rules: ["visitors"] });
 
-        var result = segment.match();
+        var result = segment.match(encounter);
 
         expect(result).toBe(true);
       });
