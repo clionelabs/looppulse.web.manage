1b3a31041fcadfd6370bc8f806e6caf6727337d3
Thomas Pun <thomas@pun.io>
2014-09-02 15:10:11 +0800

    rearranged signature for Benchmark.tiem() #78

--- server/main.js
+++ server/main.js
@@ -3,17 +3,17 @@ configure();
 Meteor.startup(
   Meteor.bindEnvironment(function() {
     Meteor.defer(function() {
-      Benchmark.time("[Startup] configure",         configureDEBUG);
-      Benchmark.time("[Startup] ensureIndexes",     ensureIndexes);
-      Benchmark.time("[Startup] observeFirebase",   observeFirebase);
-      Benchmark.time("[Startup] observeCollections",observeCollections);
+      Benchmark.time(configureDEBUG, "[Startup] configure");
+      Benchmark.time(ensureIndexes, "[Startup] ensureIndexes");
+      Benchmark.time(observeFirebase, "[Startup] observeFirebase");
+      Benchmark.time(observeCollections, "[Startup] observeCollections");
     });
   })
 );
 
 var observeFirebase = function() {
-  Benchmark.time("[Startup] observeBeaconEventsFromFirebase", observeBeaconEventsFromFirebase);
-  Benchmark.time("[Startup] observeEngagementEventsFromFirebase", observeEngagementEventsFromFirebase);
+  Benchmark.time(observeBeaconEventsFromFirebase, "[Startup] observeBeaconEventsFromFirebase");
+  Benchmark.time(observeEngagementEventsFromFirebase, "[Startup] observeEngagementEventsFromFirebase");
 };
 
 var observeCollections = function() {
