13d72f482109b8ee62bde4c284efeb2371d3d8d6
Thomas Pun <thomas@pun.io>
2014-09-02 14:55:30 +0800

    Benchmark.time(). Close #78

--- server/main.js
+++ server/main.js
@@ -3,30 +3,17 @@ configure();
 Meteor.startup(
   Meteor.bindEnvironment(function() {
     Meteor.defer(function() {
-      console.time("[startup] configure");
-      configureDEBUG();
-      console.timeEnd("[startup] configure");
-
-      console.time("[startup] ensureIndexes");
-      ensureIndexes();
-      console.timeEnd("[startup] ensureIndexes");
-
-      observeFirebase();
-
-      console.time("[startup] observeCollections");
-      observeCollections();
-      console.timeEnd("[startup] observeCollections");
+      Benchmark.time("[Startup] configure",         configureDEBUG);
+      Benchmark.time("[Startup] ensureIndexes",     ensureIndexes);
+      Benchmark.time("[Startup] observeFirebase",   observeFirebase);
+      Benchmark.time("[Startup] observeCollections",observeCollections);
     });
   })
 );
 
 var observeFirebase = function() {
-  console.time("[startup] observeBeaconEventsFromFirebase");
-  observeBeaconEventsFromFirebase();
-  console.timeEnd("[startup] observeBeaconEventsFromFirebase");
-  console.time("[startup] observeEngagementEventsFromFirebase");
-  observeEngagementEventsFromFirebase();
-  console.timeEnd("[startup] observeEngagementEventsFromFirebase");
+  Benchmark.time("[Startup] observeBeaconEventsFromFirebase", observeBeaconEventsFromFirebase);
+  Benchmark.time("[Startup] observeEngagementEventsFromFirebase", observeEngagementEventsFromFirebase);
 };
 
 var observeCollections = function() {
