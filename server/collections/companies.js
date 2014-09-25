efef8aaed73fccbd26b24d16cd3d39b11ad94819
Ratsolean <rat.so.nlp+github@gmail.com>
2014-09-25 12:55:12 +0800

    Generate Firebase token from secret

--- server/collections/companies.js
+++ server/collections/companies.js
@@ -13,9 +13,15 @@ Company.prototype.generateEngagementEventsRef = function() {
 // This is a JSON returned after successfully authenticated
 Company.prototype.authenticatedResponse = function() {
   var systemConfig = this.systemConfig;
+  var tokenGenerator = new FirebaseTokenGenerator(systemConfig.firebase.rootSecret);
+  var token = tokenGenerator.createToken(
+    {companyId: this._id},
+    {admin: true}  // FIXME config Firebase security role per company
+  );
   _.extend(systemConfig, {
     "configurationJSON": this.configurationJSON,
     "firebase": {
+      "token": token,
       "beacon_events": this.generateBeaconEventsRef(),
       "engagement_events": this.generateEngagementEventsRef()
     }
