f33021bef35c522440ef5cfd7953df071f2ec878
Ratsolean <rat.so.nlp+github@gmail.com>
2014-10-07 16:35:37 +0800

    Remove no longer using helper

--- client/views/segment/segment_list.js
+++ client/views/segment/segment_list.js
@@ -1,10 +1,2 @@
 Template.segmentList.helpers({
-  //one company per user for now
-  companyName: function () {
-    if (Companies && Companies.findOne()) {
-      return Companies.findOne().name;
-    } else {
-      return null;
-    }
-  }
 });
