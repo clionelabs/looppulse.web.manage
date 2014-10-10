fecc095cb2a5facf4e62eb3d21534461108aba9c
Wat Chun Pang Gilbert <gilbert.wat@getthingsdone.hk>
2014-10-10 18:45:12 +0800

    Fixed formatting

--- client/views/segment/segment_list.js
+++ client/views/segment/segment_list.js
@@ -1,2 +1,21 @@
 Template.segmentList.helpers({
+  isLargerThanThousand : function(num) {
+    return num > 1000;
+  },
+  isTimeLargerThanMinute : function(num) {
+    return num > 60000;
+  },
+  formatThousand : function(num) {
+    return numeral(num).format('0.00a');
+  },
+  formatPercent : function(num) {
+    return numeral(num).format('0.00%');
+  },
+  formatDurationToMin : function(num) {
+    return moment(num).diff(moment(0), 'minutes') + 'min';
+  },
+  formatDurationToSec : function(num) {
+    return moment(num).diff(moment(0), 'seconds') + 'sec';
+  }
+  
 });
