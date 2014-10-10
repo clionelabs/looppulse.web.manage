2d8bcbba1d73cdf2923fceb1ac14d394da6ee289
Ratsolean <rat.so.nlp+github@gmail.com>
2014-10-10 16:27:04 +0800

    Utilize companyId in Visitor

--- server/collections/segments.js
+++ server/collections/segments.js
@@ -5,14 +5,11 @@
  * @returns {boolean}
  */
 Segment.prototype.match = function (visitorId, encounter) {
+  // TODO remove optional `encounter` from arguments, also visitor instance could be passed in as visitor instance was fetched during daily jobs
   var self = this;
 
-  // Make sure visitor has encounter of segment.companyId
-  if (encounter) {
-    if (!encounter.didHappenInCompany(self.companyId)) {
-      return false;
-    }
-  } else if (!Encounters.findOneByCompany(self.companyId, { visitorId: visitorId })) {
+  // Make sure visitor is the same company of segment
+  if (!Visitors.findOne({ _id: visitorId, companyId: self.companyId})) {
     return false;
   }
 
