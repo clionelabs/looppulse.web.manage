236ed6cb900b05c80e6ef91c85647f79e0870f2c
Wat Chun Pang Gilbert <gilbert.wat@getthingsdone.hk>
2014-10-10 19:08:55 +0800

    Fixed style problem

--- client/views/segment/segment_list.js
+++ client/views/segment/segment_list.js
@@ -1,21 +1,20 @@
 Template.segmentList.helpers({
-  isLargerThanThousand : function(num) {
-    return num > 1000;
-  },
-  isTimeLargerThanMinute : function(num) {
-    return num > 60000;
-  },
   formatThousand : function(num) {
-    return numeral(num).format('0.00a');
+    if (num > 1000) {
+      return numeral(num).format('0.00a');
+    } else {
+      return numberal(num).format('0.0');
+    }
   },
   formatPercent : function(num) {
     return numeral(num).format('0.00%');
   },
   formatDurationToMin : function(num) {
-    return moment(num).diff(moment(0), 'minutes') + 'min';
-  },
-  formatDurationToSec : function(num) {
-    return moment(num).diff(moment(0), 'seconds') + 'sec';
+    if (num > 60000) {
+      return moment(num).diff(moment(0), 'minutes') + 'min';
+    } else {
+      return moment(num).diff(moment(0), 'seconds') + 'sec';
+    }
   }
   
 });
