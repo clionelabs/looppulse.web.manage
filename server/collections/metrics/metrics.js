2c73e6f2a8930695daf2074e0943d10afcb1eecb
Wat Chun Pang Gilbert <gilbert.wat@getthingsdone.hk>
2014-10-27 13:22:49 +0800

    Added list data generator in SegmentMetric

---
+++ server/collections/metrics/metrics.js
@@ -0,0 +1,10 @@
+Metric.generateAllSegmentRelated = function(userId, from, to) {
+    var companyId = Companies.findOne({ownedByUserIds : userId })._id;
+    Segment.find({companyId: companyId}).map(function(segment) {
+        SegmentMetric.generateAllGraph(segment, from, to);
+    });
+}
+
+Metrics.removeAllFromUser = function(userId) {
+    Metrics.remove({userId: userId});
+}
--- server/collections/metrics/segment_metrics.js
+++ server/collections/metrics/segment_metrics.js
@@ -2,7 +2,7 @@ SegmentMetrics = {};
 
 SegmentMetrics.findList = function() {
     var companyId = Companies.findOne({ownedByUserId : this.userId});
-    return Metrics.find({type: "segment", companyId : companyId, graphType : GraphDataParser.Type.List});
+    return Metrics.find({type: "segment", companyId : companyId, graphType : SegmentMetric.Graph.List});
 };
 
 SegmentMetric = {};
@@ -26,70 +26,137 @@ SegmentMetric.TimeBucket.Day = "day";
 SegmentMetric.TimeBucket.Week = "week";
 SegmentMetric.TimeBucket.Month = "month";
 
-SegmentMetric.generateAllGraph = function(segmentId, from, to) {
+SegmentMetric.generateAllGraph = function(segment, from, to) {
     //TODO get visitorIds from kim's work
     var visitorIds = [];
-    var encounters = Encounters.findClosedByVisitors(visitorIds);
+    var encounters = Encounters.findClosedByVisitorsInTimePeriod(visitorIds, from, to);
     //TODO get visitors: [segmentIds] kim's work
     var visitorInSegmentsHash = {};
     //TODO get visitors: [tags]
     var visitorHasTagsHash = {};
 
-    prepareListData(encounters);
+    var listData = SegmentMetric.prepareListData(encounters);
+    var collectionMeta = new Metric.CollectionMeta(segment._id, Metric.CollectionMeta.Type.Segment);
+    var listMetricSelector = {
+        companyId: segment.companyId,
+        collectionMeta: collectionMeta,
+        from: from,
+        to: to,
+        graphType: SegmentMetric.Graph.List
+    };
+    var listMetric = new Metric(segment.companyId, collectionMeta, from, to, SegmentMetric.Graph.List, listData);
+    Metric.upsert(listMetricSelector, listMetric);
 
-    prepareTimeBucketXNumOfVisitorHistogramData(SegmentMetric.TimeBucket.Week, encounters);
-    prepareVisitorOtherSegmentsBarChartData(visitorInSegmentsHash);
-    prepareVisitorsTagsBarChartData(visitorHasTagsHash);
 
-    prepareAverageDwelTimeBucketXNumOfVisitorHistogramData(encounters);
-    prepareDwellTimeInTimeFrameBubbleData(encounters);
+    SegmentMetric.prepareTimeBucketXNumOfVisitorHistogramData(SegmentMetric.TimeBucket.Week, encounters);
+    SegmentMetric.prepareVisitorOtherSegmentsBarChartData(visitorInSegmentsHash);
+    SegmentMetric.prepareVisitorsTagsBarChartData(visitorHasTagsHash);
 
-    prepareNumberOfVisitXNumberOfVisitorsHistogramData(encounters);
-    prepareNumberOfVisitsInTimeFrameBubbleData(encounters);
+    SegmentMetric.prepareAverageDwelTimeBucketXNumOfVisitorHistogramData(encounters);
+    SegmentMetric.prepareDwellTimeInTimeFrameBubbleData(encounters);
+
+    SegmentMetric.prepareNumberOfVisitXNumberOfVisitorsHistogramData(encounters);
+    SegmentMetric.prepareNumberOfVisitsInTimeFrameBubbleData(encounters);
 
 }
 
-var prepareListData = function(encounters) {
+SegmentMetric.prepareListData = function(encounters) {
     console.log("[SegmentMetric] generating list view data");
-    var grpByVisitors = _.groupBy(encounters, function(e) {
+    var listData = {
+        numberOfVisitors : 0,
+        averageDwellTime : 0,
+        repeatedVisitorPercentage : 0
+    };
+
+    if (!encounters.length) {
+        return listData;
+    } else {
+        var grpByVisitors = _.groupBy(encounters, function (e) {
         return e.visitorId;
-    });
-    console.log(JSON.stringify(grpByVisitors));
-    //TODO add impl
+        });
+        console.log("1 " + JSON.stringify(grpByVisitors));
+        grpByVisitors = _.map(grpByVisitors, function(visitors, id) {
+            var result = {};
+            var groupedEncounters = _.groupBy(visitors, function(encounter) {
+                return moment(encounter.enteredAt).format("YYYY-MM-DD");
+            });
+            result[id.toString()] = groupedEncounters;
+            return result;
+        });
+        //convert back to hash
+        grpByVisitors = _.reduce(grpByVisitors, function(memo, visitor) {
+            return _.extend(memo, visitor);
+        }, {});
+        console.log("2 " + JSON.stringify(grpByVisitors));
+
+        listData.numberOfVisitors =  _.size(grpByVisitors);
+        console.log("After calculating number of visitors: " + JSON.stringify(listData));
+
+        var getRepeated = function(encountersByDate) {
+            return _.size(encountersByDate) > 1;
+        };
+        var numberOfRepeatedVisitor = _.size(_.filter(grpByVisitors, getRepeated));
+        listData.repeatedVisitorPercentage = numberOfRepeatedVisitor / listData.numberOfVisitors;
+        console.log("After calculating percentage of repeated visit" + JSON.stringify(listData));
+
+        var getTotalDwellTime = function(encounters) {
+            return _.reduce(encounters, function(memo, encounter) {
+                return memo + encounter.duration;
+            }, 0);
+        };
+        var getAverageDwellTimePerDay = function(encountersByDate) {
+            return _.reduce(encountersByDate, function (memo, encounters) {
+               return memo + getTotalDwellTime(encounters);
+            }, 0) / _.size(encountersByDate);
+        }
+        var getAverageDwellTimePerVisitor = function(visitorEncountersByDate) {
+            return _.reduce(visitorEncountersByDate, function (memo, encountersByDate) {
+                    return memo + getAverageDwellTimePerDay(encountersByDate);
+                }, 0) / _.size(visitorEncountersByDate);
+        }
+        listData.averageDwellTime = getAverageDwellTimePerVisitor(grpByVisitors);
+        console.log(JSON.stringify(listData));
+
+        return listData;
+    }
 }
 
-var prepareTimeBucketXNumOfVisitorHistogramData = function(bucketSize, encounters) {
+SegmentMetric.prepareTimeBucketXNumOfVisitorHistogramData = function(bucketSize, encounters) {
     console.log("[SegmentMetric] generating time bucket against number of visitors histogram");
     //TODO add impl
 }
 
-var prepareVisitorOtherSegmentsBarChartData = function(visitorInSegmentsHash) {
+SegmentMetric.prepareVisitorOtherSegmentsBarChartData = function(visitorInSegmentsHash) {
     console.log("[SegmentMetric] generating other segment percentage var chart");
     //TODO add impl
 }
 
-var prepareVisitorsTagsBarChartData = function(visitorHasTagsHash) {
+SegmentMetric.prepareVisitorsTagsBarChartData = function(visitorHasTagsHash) {
     console.log("[SegmentMetric] generating visitors tag percentage bar chart");
     //TODO add impl
 }
 
-var prepareAverageDwelTimeBucketXNumOfVisitorHistogramData = function(encounters) {
+SegmentMetric.prepareAverageDwelTimeBucketXNumOfVisitorHistogramData = function(encounters) {
     console.log("[SegmentMetric] preparing average dwell time again number of Visitors histogram");
     //TODO add impl
 
 }
 
-var prepareDwellTimeInTimeFrameBubbleData = function(encounters) {
+SegmentMetric.prepareDwellTimeInTimeFrameBubbleData = function(encounters) {
     console.log("[SegmentMetric] preparing dwell time in time frame bubble data");
     //TODO add impl
 }
 
-var prepareNumberOfVisitXNumberOfVisitorsHistogramData = function(encounters) {
+SegmentMetric.prepareNumberOfVisitXNumberOfVisitorsHistogramData = function(encounters) {
     console.log("[SegmentMetric] preparing number of visit against number of visitors");
     //TODO add impl
 }
 
-var prepareNumberOfVisitsInTimeFrameBubbleData = function(encounters) {
+SegmentMetric.prepareNumberOfVisitsInTimeFrameBubbleData = function(encounters) {
     console.log("[SegmentMetric] preparing number of visits in time frame bubble data");
     //TODO add impl
 }
+
+Meteor.startup = function() {
+    //TODO observe spring
+};
