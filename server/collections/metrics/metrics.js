0636a2267735cd1b19bb1ade030123308c195b81
Wat Chun Pang, Gilbert <gilbert.wat@getthingsdone.hk>
2014-11-18 17:06:58 +0800

    Reduce segment detail page to only reload the particular segment metrics

--- server/collections/metrics/metrics.js
+++ server/collections/metrics/metrics.js
@@ -1,11 +1,20 @@
-Metric.generateAllSegmentRelated = function(userId, from, to) {
+Metric.generateAllSegmentsGraph = function (userId, from, to) {
     var companyId = Companies.findOne({ownedByUserIds : userId })._id;
     Segments.find({companyId: companyId}).map(function(segment) {
         SegmentMetric.generateAllGraph(segment, from, to);
     });
 };
 
+Metric.generateAllGraph = function(userId, segmentId, from, to) {
+    var companyId = Companies.findOne({ownedByUserIds : userId })._id;
+    var segment = new Segment(Segments.findOne({companyId: companyId, _id : segmentId}));
+
+    SegmentMetric.generateAllGraph(segment, from, to);
+
+}
+
 Metrics.removeAllFromCompany = function(companyId) {
+    console.log("Removing segment metrics from company " + companyId);
     Metrics.remove({companyId: companyId});
 };
 
